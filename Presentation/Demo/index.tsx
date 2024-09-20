4710d1691b65c6473cbcafe70a27bdc325021a3a
Anton Myasnikov <hanjimonor@gmail.com>
2024-09-20 16:57:54 +0400

    Styles: some very minor cosmetics changes

--- Presentation/Demo/index.tsx
+++ Presentation/Demo/index.tsx
@@ -6,6 +6,7 @@ import clsx from "clsx"
 import Button from "@/ui/Actions/Button"
 import Brick from "@/ui/Layout/Brick"
 import Code from "@/ui/Presentation/Code"
+import Text from "@/ui/Presentation/Text"
 // Styles and types
 import styles from "./styles.module.scss"
 import { DemoProps } from "./types"
@@ -17,8 +18,8 @@ function Demo({ children, className, code, withoutCopy }: DemoProps) {
     <Brick className={calculatedClassNames}>
       <div className={styles["demo-content"]}>{children}</div>
       <div className={styles["demo-action-panel"]}>
-        <Button onClick={() => setShowCode(!showCode)} blue transparent>
-          {showCode ? "Hide code" : "Show code"}
+        <Button className={styles["demo-code-button"]} onClick={() => setShowCode(!showCode)} blue transparent>
+          <Text type="fit-line" size="small">{showCode ? "Hide code" : "Show code"}</Text>
         </Button>
       </div>
       {showCode && <Code className={styles["demo-code"]} code={code} withoutCopy={withoutCopy} />}
