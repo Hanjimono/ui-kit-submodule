62fd31db7b88d20e703cb5c4864fe4719d336d95
Anton Myasnikov <hanjimonor@gmail.com>
2024-10-14 15:47:25 +0400

    Testing husky for ui-submodule

--- Form/FormSubmit/index.tsx
+++ Form/FormSubmit/index.tsx
@@ -3,6 +3,7 @@ import Button from "@/ui/Actions/Button"
 // Styles and types
 import { FormSubmitProps } from "./types"
 
+/** Special button for form submitting */
 function FormSubmit({ onSubmit, children, ...rest }: FormSubmitProps) {
   return (
     <Button onClick={onSubmit} {...rest}>
