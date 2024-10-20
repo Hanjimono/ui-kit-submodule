f33a00c719747084845d2271ffe4a42210714d69
Anton Myasnikov <hanjimonor@gmail.com>
2024-10-20 16:48:00 +0400

    Presentation: add tooltip component

---
+++ Presentation/Tooltip/index.tsx
@@ -0,0 +1,84 @@
+// System
+import clsx from "clsx"
+import { Tooltip as ReactTooltip } from "react-tooltip"
+// Ui
+import Icon from "@/ui/Presentation/Icon"
+// Styles and types
+import { TooltipIconProps, TooltipProps } from "./types"
+import styles from "./styles.module.scss"
+
+/**
+ * Tooltip component that displays a tooltip when hovering over the child elements.
+ *
+ * @param {React.ReactNode} children - The content that will trigger the tooltip on hover.
+ * @param {string} [className] - Additional class names to apply to the tooltip container.
+ * @param {string} [tooltipId] - Unique identifier for the tooltip. If not provided, a random ID will be generated.
+ * @param {string} tooltip - The content to display inside the tooltip.
+ * @param {string} [place="right"] - The position of the tooltip relative to the target element. Defaults to "right".
+ * @param {string} [variant="dark"] - The visual style of the tooltip. Defaults to "dark".
+ * @param {boolean} [styled] - Flag to apply additional styling to the tooltip.
+ *
+ * @returns {JSX.Element} The rendered Tooltip component.
+ */
+export function Tooltip({
+  children,
+  className,
+  tooltipId,
+  tooltip,
+  place = "right",
+  variant = "dark",
+  styled
+}: TooltipProps) {
+  if (!tooltipId) {
+    tooltipId = Math.random().toString(36).substr(2, 9)
+  }
+  const calculatedClassNames = clsx(
+    styles["tooltip"],
+    className,
+    styled && styles["styled"]
+  )
+  return (
+    <span
+      data-tooltip-id={tooltipId}
+      data-tooltip-place={place}
+      data-tooltip-offset={10}
+      className={calculatedClassNames}
+      data-tooltip-variant={variant}
+    >
+      {children}
+      <ReactTooltip id={tooltipId}>{tooltip}</ReactTooltip>
+    </span>
+  )
+}
+export default Tooltip
+
+/**
+ * TooltipIcon component renders an icon inside a tooltip.
+ *
+ * @param {string} [icon="help"] - The name of the icon to be displayed.
+ * @param {string} [iconType="md"] - The type of the icon (e.g., material design).
+ * @param {number} [iconSize=18] - The width of the icon.
+ * @param {number} [iconHeight=18] - The height of the icon.
+ * @param {TooltipIconProps} rest - Additional props to be passed to the Tooltip component.
+ * @returns {JSX.Element} The rendered TooltipIcon component.
+ */
+export function TooltipIcon({
+  icon = "help",
+  iconType = "md",
+  iconSize = 18,
+  iconHeight = 18,
+  ...rest
+}: TooltipIconProps) {
+  return (
+    <Tooltip {...rest}>
+      <Icon
+        name={icon}
+        alt={icon}
+        type={iconType}
+        customIconLink={icon}
+        width={iconSize}
+        height={iconHeight}
+      />
+    </Tooltip>
+  )
+}
---
+++ Presentation/Tooltip/types.ts
@@ -0,0 +1,32 @@
+import { PlacesType, VariantType } from "react-tooltip"
+import { IconType } from "@/ui/Presentation/Icon/types"
+
+/** Tooltip component that displays a tooltip when hovering over the child elements. */
+export interface TooltipProps {
+  /** React children */
+  children?: React.ReactNode
+  /** Additional CSS classes */
+  className?: string
+  /** Tooltip content */
+  tooltip: string | React.ReactNode
+  /** Unique identifier for the tooltip */
+  tooltipId?: string
+  /** Position of the tooltip */
+  place?: PlacesType
+  /** Variant of the tooltip */
+  variant?: VariantType
+  /** Whether the tooltip is styled */
+  styled?: boolean
+}
+
+/** TooltipIcon component renders an icon inside a tooltip. */
+export interface TooltipIconProps extends TooltipProps {
+  /** Icon name */
+  icon?: string
+  /** Icon size */
+  iconSize?: number
+  /** Icon height */
+  iconHeight?: number
+  /** Type of the icon */
+  iconType?: IconType
+}
